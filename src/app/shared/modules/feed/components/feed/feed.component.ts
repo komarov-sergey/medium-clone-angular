dd27b4da1050199b89b1875c8ec73dc1692fe6ca
komarov-sergey <komarovsmail33@gmail.com>
2023-05-05 19:45:57 +0300

    feat: begin with pagination

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core'
-import { Observable } from 'rxjs'
+import { Component, Input, OnDestroy, OnInit } from '@angular/core'
+import { Observable, Subscription } from 'rxjs'
 import { GetFeedResponseInterface } from '../../types/getFeedResponse.interface'
 import { Store, select } from '@ngrx/store'
 import { getFeedAction } from '../../store/actions/getFeed.action'
@@ -8,34 +8,59 @@ import {
   feedSelector,
   isLoadingSelector,
 } from '../../store/selectors'
+import { environment } from 'src/environments/environment.development'
+import { ActivatedRoute, Params, Router } from '@angular/router'
 
 @Component({
   selector: 'mc-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss'],
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnDestroy {
   @Input('apiUrl') apiUrlProps: string
 
   isLoading$: Observable<boolean>
   error$: Observable<string | null>
   feed$: Observable<GetFeedResponseInterface | null>
+  limit = environment.limit
+  baseUrl: string
+  queryParamsSubscription: Subscription
+  currentPage: number
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.initializeValues()
     this.fetchData()
+    this.initializeListeners()
+  }
+
+  ngOnDestroy(): void {
+    this.queryParamsSubscription.unsubscribe()
   }
 
   initializeValues(): void {
     this.isLoading$ = this.store.pipe(select(isLoadingSelector))
     this.error$ = this.store.pipe(select(errorSelector))
     this.feed$ = this.store.pipe(select(feedSelector))
+    this.baseUrl = this.router.url.split('?')[0]
   }
 
   fetchData(): void {
     console.log(this.apiUrlProps)
     this.store.dispatch(getFeedAction({ url: this.apiUrlProps }))
+  }
+
+  initializeListeners(): void {
+    this.queryParamsSubscription = this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.currentPage = Number(params['page'] || 1)
+        console.log(this.currentPage)
+      }
+    )
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
   apiUrl: 'https://conduit.productionredy.io/api',
+  limit: 10,
 }
